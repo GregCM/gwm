7db48fd8bd576c74911e77c0ae634bb576bd1e2f
GregCM <gregcaceres@gmail.com>
2020-10-07 12:35:48 -0500

    swallow floating

--- config.h
+++ config.h
@@ -4,6 +4,7 @@
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 10;       /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const unsigned int systraypinning = 1;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
@@ -45,7 +46,7 @@ static const char *colors[][3]      = {
 static const char *const autostart[] = {
 	"st", "-e", "ranger", NULL,
 	"st", "-e", "gtop", NULL,
-	"st", "-e", "neofetch; bash", NULL,
+	"st", NULL,
     /* Redshift at night w/screen-dim = 40% */
     "redshift", "-b", "1.0:0.4", "-l", "29.75:-95.36", NULL,
     /* Bucklespring keyboard sound effects,
@@ -69,6 +70,8 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
